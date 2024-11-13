a2291f09929c693b964adbbd67e2fca8ded41115
huntesta349 <huntesta349@SDS-L-79QLG63>
2024-11-13 10:17:57 -0500

    Add project files.

---
+++ HouseOfCassieGame/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace HouseOfCassieGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureDisplay = new System.Windows.Forms.PictureBox();
+            this.leftButton = new System.Windows.Forms.Button();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.storyLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureDisplay
+            // 
+            this.pictureDisplay.Image = global::HouseOfCassieGame.Properties.Resources.page1;
+            this.pictureDisplay.Location = new System.Drawing.Point(13, -4);
+            this.pictureDisplay.Name = "pictureDisplay";
+            this.pictureDisplay.Size = new System.Drawing.Size(775, 417);
+            this.pictureDisplay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureDisplay.TabIndex = 0;
+            this.pictureDisplay.TabStop = false;
+            // 
+            // leftButton
+            // 
+            this.leftButton.Location = new System.Drawing.Point(13, 459);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(180, 111);
+            this.leftButton.TabIndex = 1;
+            this.leftButton.UseVisualStyleBackColor = true;
+            this.leftButton.Click += new System.EventHandler(this.leftButton_Click);
+            // 
+            // rightButton
+            // 
+            this.rightButton.Location = new System.Drawing.Point(608, 459);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(180, 111);
+            this.rightButton.TabIndex = 2;
+            this.rightButton.UseVisualStyleBackColor = true;
+            this.rightButton.Click += new System.EventHandler(this.rightButton_Click);
+            // 
+            // storyLabel
+            // 
+            this.storyLabel.AutoSize = true;
+            this.storyLabel.Location = new System.Drawing.Point(10, 427);
+            this.storyLabel.Name = "storyLabel";
+            this.storyLabel.Size = new System.Drawing.Size(0, 13);
+            this.storyLabel.TabIndex = 3;
+            this.storyLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 611);
+            this.Controls.Add(this.storyLabel);
+            this.Controls.Add(this.rightButton);
+            this.Controls.Add(this.leftButton);
+            this.Controls.Add(this.pictureDisplay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "The House of Cassie";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureDisplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureDisplay;
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.Label storyLabel;
+    }
+}
+
---
+++ HouseOfCassieGame/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace HouseOfCassieGame
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+        SoundPlayer sp = new SoundPlayer(Properties.Resources.gamelose);
+        
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            SoundPlayer sp = new SoundPlayer(Properties.Resources.funnywin);
+            
+            rightButton.Visible = false;
+
+            pictureDisplay.Image = Properties.Resources.page1;
+
+            leftButton.Text = "Continue?";
+
+            storyLabel.Text = "Tyler walks into a haunted house to find out the mystery of Cassie's disappearance in the 1920's";
+
+        }
+
+        private void leftButton_Click(object sender, EventArgs e)
+        {
+            rightButton.Visible = true;
+
+            if (page == 1)
+            {
+                storyLabel.Text = "Should Tyler go up the stairs of into the dark basement?";
+                leftButton.Text = "He should go up the stairs.";
+                rightButton.Text = "He should go into the dark basement.";
+                pictureDisplay.Image = Properties.Resources.page2;
+                page = 2;
+                
+            }
+            else if (page == 2)
+            {
+                Random ranGen = new Random();
+                int randomNumber;
+
+                randomNumber = ranGen.Next(1, 101);
+
+                if (randomNumber > 20)
+                {
+                    storyLabel.Text = "OH NO! Tyler accidentilly trips an axe trap and dies. Game over.";
+                    leftButton.Text = "Try again.";
+                    rightButton.Visible = false;
+                    pictureDisplay.Image = Properties.Resources.page4and12;
+                    page = 4;
+                    sp = new SoundPlayer(Properties.Resources.gamelose);
+                    sp.Play();
+                }
+                if (randomNumber < 20)
+                {
+                    storyLabel.Text = "Tyler makes it up the stairs and he meets Cassie. They talk and Cassie tells Tyler how she mysteriously dissapeared! The end.";
+                    leftButton.Text = "Play again.";
+                    rightButton.Visible = false;
+                    pictureDisplay.Image = Properties.Resources.page5;
+                    page = 5;
+                    sp = new SoundPlayer(Properties.Resources.funnywin);
+                    sp.Play();
+                }
+            }
+            else if (page == 3)
+            {
+                storyLabel.Text = "OH NO! Tyler reads the diary and is killed by Cassie! Game over.";
+                leftButton.Text = "Try again.";
+                rightButton.Visible = false;
+                pictureDisplay.Image = Properties.Resources.page6and10;
+                page = 6;
+                sp = new SoundPlayer(Properties.Resources.gamelose);
+                sp.Play();
+            }
+            else if (page == 4)
+            {
+                storyLabel.Text = "Tyler walks into a haunted house to find out the mystery of Cassie's disappearance in the 1920's";
+                leftButton.Text = "Continue?";
+                rightButton.Visible = false;
+                pictureDisplay.Image = Properties.Resources.page1;
+                page = 1;
+            }
+            else if (page == 5)
+            {
+                storyLabel.Text = "Tyler walks into a haunted house to find out the mystery of Cassie's disappearance in the 1920's";
+                leftButton.Text = "Continue?";
+                rightButton.Visible = false;
+                pictureDisplay.Image = Properties.Resources.page1;
+                page = 1;
+            }
+            else if (page == 6)
+            {
+                storyLabel.Text = "Tyler walks into a haunted house to find out the mystery of Cassie's disappearance in the 1920's";
+                leftButton.Text = "Continue?";
+                rightButton.Visible = false;
+                pictureDisplay.Image = Properties.Resources.page1;
+                page = 1;
+            }
+            else if (page == 7)
+            {
+                storyLabel.Text = "OH NO! Tyler gets bit by a poisonous spider and dies! Game over.";
+                leftButton.Text = "Try again.";
+                rightButton.Visible = false;
+                pictureDisplay.Image = Properties.Resources.page8;
+                page = 8;
+                sp = new SoundPlayer(Properties.Resources.gamelose);
+                sp.Play();
+            }
+            else if (page == 8)
+            {
+                storyLabel.Text = "Tyler walks into a haunted house to find out the mystery of Cassie's disappearance in the 1920's";
+                leftButton.Text = "Continue?";
+                rightButton.Visible = false;
+                pictureDisplay.Image = Properties.Resources.page1;
+                page = 1;
+            }
+            else if (page == 9)
+            {
+                storyLabel.Text = "Uh oh! The front door is locked! Should Tyler look into the dark kitchen for a key or sould he look upstairs?";
+                leftButton.Text = "He should look in the dark kitchen.";
+                rightButton.Text = "He should look upstairs.";
+                pictureDisplay.Image = Properties.Resources.page11;
+                page = 11;
+            }
+            else if (page == 10)
+            {
+                storyLabel.Text = "Tyler walks into a haunted house to find out the mystery of Cassie's disappearance in the 1920's";
+                leftButton.Text = "Continue?";
+                rightButton.Visible = false;
+                pictureDisplay.Image = Properties.Resources.page1;
+                page = 1;
+
+            }
+            else if (page == 13)
+            {
+                storyLabel.Text = "Tyler walks into a haunted house to find out the mystery of Cassie's disappearance in the 1920's";
+                leftButton.Text = "Continue?";
+                rightButton.Visible = false;
+                pictureDisplay.Image = Properties.Resources.page1;
+                page = 1;
+            }
+            else if (page == 11)
+            {
+                storyLabel.Text = "Woohoo! Tyler found the key and escaped with his life and the answer to the mystery of Cassie! The end.";
+                leftButton.Text = "Play again.";
+                rightButton.Visible = false;
+                pictureDisplay.Image = Properties.Resources.page13;
+                page = 13;
+                sp = new SoundPlayer(Properties.Resources.gamewin);
+                sp.Play();
+            }
+            else if (page == 12)
+            {
+                storyLabel.Text = "Tyler walks into a haunted house to find out the mystery of Cassie's disappearance in the 1920's";
+                leftButton.Text = "Continue?";
+                rightButton.Visible = false;
+                pictureDisplay.Image = Properties.Resources.page1;
+                page = 1;
+            }
+        }
+
+        private void rightButton_Click(object sender, EventArgs e)
+        {
+            if (page == 2)
+            {
+                storyLabel.Text = "Uh oh! Tyler gets lost in the basement but he finds Cassie's diary! Should Tyler read it now or should he try to find a way out first?";
+                leftButton.Text = "He should read the diary now.";
+                rightButton.Text = "He should try to find a way out before reading the diary.";
+                pictureDisplay.Image = Properties.Resources.page3;
+                page = 3;
+                sp = new SoundPlayer(Properties.Resources.diaryfound);
+                sp.Play();
+            }
+            else if (page == 3)
+            {
+                storyLabel.Text = "Tyler starts walking and theres a fork in the road! Should Tyler go left of right?";
+                leftButton.Text = "Left.";
+                rightButton.Text = "Right.";
+                pictureDisplay.Image = Properties.Resources.page7;
+                page = 7;
+            }
+            else if (page == 7)
+            {
+                storyLabel.Text = "Tyler sees Cassie and she looks angry! Should Tyler try to make her feel better or should he keep hiding until she leaves?";
+                leftButton.Text = "He should hide until she leaves.";
+                rightButton.Text = "He should try to calm her down.";
+                pictureDisplay.Image = Properties.Resources.page9;
+                page = 9;
+            }
+            else if (page == 9)
+            {
+                storyLabel.Text = "OH NO! Cassie kills Tyler and takes her diary back! Game over.";
+                leftButton.Text = "Try again.";
+                rightButton.Visible = false;
+                pictureDisplay.Image = Properties.Resources.page6and10;
+                page = 10;
+                sp = new SoundPlayer(Properties.Resources.gamelose);
+                sp.Play();
+            }
+            else if (page == 11)
+            {
+                storyLabel.Text = "OH NO! Tyler accidentilly trips an axe trap and dies. Game over.";
+                leftButton.Text = "Try again.";
+                rightButton.Visible = false;
+                pictureDisplay.Image = Properties.Resources.page4and12;
+                page = 12;
+                sp = new SoundPlayer(Properties.Resources.gamelose);
+                sp.Play();
+
+            }
+            
+
+        }
+    }
+}
